9177dc6f1927c2257a83eab1045fa748959344ba
joowonH93 <joowon.h93@gmail.com>
2018-05-30 14:12:17 +0900

    back up 180530

---
+++ a_beginner/js/src/rwd_02.js
@@ -0,0 +1,37 @@
+// rwd_02.js
+
+(function($) {
+  // jquery
+
+// 브라우저의 가로 값을 파악
+var $w = $(window).width();
+console.log($w);
+
+// -------------------------------------------------------
+if($w <= 320){
+  console.log('모바일 세로형 형태');
+}
+
+else if ($w >= 321 && $w <= 480){
+  console.log('모바일 가로형, 타블렛 세로형')
+}
+
+else if ($w >= 481 && $w <= 768){
+  console.log('모바일 가로형, 타블렛 세로형')
+}
+
+else if ($w >= 769 && $w <= 1366){
+  console.log('타블렛 가로형, pc형')
+}
+
+else if ($w >= 1367){
+  console.log('pc full 형태')
+}
+
+// -------------------------------------------------------
+$(window).on('resize',function(){
+  location.reload();
+});
+
+
+})(jQuery);
