28716f0efd26f6be61c57cd1cc842d05ffe56312
joowonH93 <joowon.h93@gmail.com>
2018-07-19 11:45:37 +0900

    180719 미도리 메인 진행중 18일자 반응형 gnb

---
+++ b_02_jquery/js/src/rwd_gnb_01.js
@@ -0,0 +1,35 @@
+// rwd_gnb_01.js
+
+(function($){
+
+  var winW = $(window).outerWidth();
+  var gnbBtn = $('.gnb_btn');
+  var gnb = $('#gnb');
+
+  if ( winW < 480 ) {
+      gnbBtn.on('click', function(e){
+        gnb.stop().fadeToggle();
+        $(this).children('button').toggleClass('active');
+      });
+    } else if ( winW < 1366 ) {
+      gnbBtn.on('click', function(e){
+        gnb.stop().slideToggle();
+        $(this).children('button').toggleClass('active');
+      });
+  }
+
+
+
+  // --------------------------------------------
+
+  // var winW = $(window).outerWidth();
+  
+  $(window).on('resize', function(e){
+    var nowW = $(window).outerWidth();
+
+    if ( winW !== nowW ) {
+      location.reload();
+    }
+  });
+
+})(jQuery);
--- midori/js/src/midori_web.js
+++ midori/js/src/midori_web.js
@@ -2,5 +2,15 @@
 
 (function($){
 
+  var winW = $(window).outerWidth();
+  var gnbBtn = $('.gnb_btn');
+  var gnb = $('.gnb_wrap');
+
+  gnb.hide();
+
+  gnbBtn.on('click', function(e){
+    gnb.stop().fadeToggle();
+    $(this).children('button').toggleClass('active');
+  });
   
 })(jQuery);
